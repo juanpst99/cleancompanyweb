9c3393e188f019bdc2875441f5d2722a62e7b82e
juanpst99 <cleancompanymed@gmail.com>
2025-09-09 16:36:10 -0500

    Update GTMEvents.tsx

--- src/components/analytics/GTMEvents.tsx
+++ src/components/analytics/GTMEvents.tsx
@@ -1,1 +1,20 @@
+'use client'
+import { usePathname, useSearchParams } from 'next/navigation'
+import { useEffect } from 'react'
+
+export default function GTMEvents() {
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+
+  useEffect(() => {
+    const url = pathname + (searchParams?.toString() ? `?${searchParams}` : '')
+    ;(window as any).dataLayer = (window as any).dataLayer || []
+    ;(window as any).dataLayer.push({
+      event: 'pageview',
+      page_location: url,
+    })
+  }, [pathname, searchParams])
+
+  return null
+}
 
