0163b283b557b5ff5dfbbf92ce9e7435ba1f897e
juanpst99 <cleancompanymed@gmail.com>
2025-09-22 05:52:25 -0500

    feat: Actualiza dns-prefetch a preconnect para GTM/GA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -128,9 +128,9 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         
-        {/* DNS Prefetch para dominios secundarios */}
-        <link rel="dns-prefetch" href="https://www.googletagmanager.com" />
-        <link rel="dns-prefetch" href="https://www.google-analytics.com" />
+{/* Preconnect a dominios críticos de analítica */}
+<link rel="preconnect" href="https://www.googletagmanager.com" crossOrigin="" />
+<link rel="preconnect" href="https://www.google-analytics.com" crossOrigin="" />
         
         {/* Prefetch de rutas críticas */}
         <link rel="prefetch" href="/servicios/alfombras" />
